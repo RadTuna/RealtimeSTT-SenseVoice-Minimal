fbe88d783b2e82330add1b7f7442389e2e5fe7ad
Jaeseung Jeon <wotmd6180@gmail.com>
2025-03-16 18:22:01 +0900

    Added require dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
+import os
+
+# Get the absolute path of requirements.txt
+req_path = os.path.join(os.path.dirname(__file__), "requirements.txt")
+
+# Read requirements.txt safely
+with open(req_path, "r", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
 
 setup(
     name="RealtimeSTT",
     version="0.1",
     packages=find_packages(where="RealtimeSTT"),
     package_dir={"": "RealtimeSTT"},
-    install_requires=[],
+    install_requires=requirements,
 )
