489ad5dd64faba422991f09aaa0b4bd46eb3ab1b
Jaeseung Jeon <wotmd6180@gmail.com>
2025-03-16 17:55:16 +0900

    Modified package structure for using local package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RealtimeSTT",
+    version="0.1",
+    packages=find_packages(where="RealtimeSTT"),
+    package_dir={"": "RealtimeSTT"},
+    install_requires=[],
+)
